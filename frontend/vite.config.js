dbbf3a2c5b07919a2b9883ce9e12228a17b4c018
Dennis Miring'u <167270355+markdennis1287@users.noreply.github.com>
2025-02-08 10:17:55 +0000

    Deployment issues fix trial 1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': 'https://my-portfolio-v2-v9w0.onrender.com',
     },
   },
 });
