0c358dee0432a9bbd7ee98e7ce3f497d7001c141
Dennis Miring'u <167270355+markdennis1287@users.noreply.github.com>
2025-02-08 09:44:31 +0000

    Server update-2

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,5 @@ app.use(express.json());
 
 app.use('/api', require('./routes/api'));
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-// Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
-
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
